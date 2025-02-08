4556ef08cf70823bd799cb4a7124c6bdb529c059
PumpkinBrain <PumpkinBrain@users.noreply.github.com>
2025-02-08 19:01:37 -0300

    basic project struture and start of homepage

---
+++ src/components/sections/AboutMe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const AboutMeSection: React.FC = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img src="/profilepic.jpg"></img>
+      <p>blablabalbal</p>
+    </div>
+  );
+};
+
+export default AboutMeSection;
