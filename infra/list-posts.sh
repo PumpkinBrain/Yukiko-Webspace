2084e95f6b5d8e5a53842df698b3ab2e98bb7a9e
yukiko <dmcalmeida@outlook.com>
2025-05-26 07:40:10 -0300

    a

--- infra/list-posts.sh
+++ infra/list-posts.sh
@@ -3,18 +3,22 @@ destination=data/posts.json
 
 echo "" >$destination
 echo "[" >>$destination
+ID=0
+
 for file in blogs/*; do
   title=$(grep -E '^\[metadata\.title\]:#' $file | sed 's/^[^"]*"/"/')
   tag=$(grep -E '^\[metadata\.tag\]:#' $file | sed 's/^[^"]*"/"/')
   date=$(grep -E '^\[metadata\.date\]:#' $file | sed 's/^[^"]*"/"/')
   description=$(grep -E '^\[metadata\.description\]:#' $file | sed 's/^[^"]*"/"/')
   echo "{
+  \"id\": $ID,
   \"title\": $title,
   \"tag\": $tag,
   \"date\": $date,
   \"file\": \"$file\",
   \"description\": $description
 }," >>$destination
+  ID=$(($ID + 1))
 done
 echo "]" >>$destination
 sed -i ':a;N;$!ba;s/\n//g' $destination &&
--- src/domain/entities/BlogPost.ts
+++ src/domain/entities/BlogPost.ts
@@ -1,11 +1,13 @@
 export default class BlogPost {
+  id: number;
   title: string;
   tag: string;
   date: string;
   file: string;
   description: string;
 
-  constructor(title: string, tag: string, date: string, file: string, description: string) {
+  constructor(id: number, title: string, tag: string, date: string, file: string, description: string) {
+    this.id = id;
     this.title = title;
     this.tag = tag;
     this.date = date;
