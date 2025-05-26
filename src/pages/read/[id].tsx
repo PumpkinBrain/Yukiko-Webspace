1c9aafeaeffc82ff4d3438655f87c7bfdeab0167
yukiko <dmcalmeida@outlook.com>
2025-05-26 08:28:11 -0300

    fixing static generation

--- src/pages/read/[id].tsx
+++ src/pages/read/[id].tsx
@@ -1,24 +1,39 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../layout';
+import data from "../../../public/data/posts.json"
+import BlogPost from '@/domain/entities/BlogPost';
 
-const ReadingPage: React.FC = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  if (!id) {
+interface props {
+  id: number;
+}
+interface Iparam {
+  params: {
+    id: string
+  }
+}
+
+const ReadingPage: React.FC<props> = (props: props) => {
+  if (!props.id) {
     return <p>loading...</p>
   }
   return (
     <Layout>
       <h1>Hello world!</h1>
-      <p>reading id {id}</p>
-      <h1>{id}</h1>
+      <p>reading id {props.id}</p>
+      <h1>{props.id}</h1>
     </Layout>
   )
 }
 
-export async function generateStaticParams() {
-  return [{ id: "1" }, { id: "test" }]
+export async function getStaticPaths() {
+  const paths = data.map((post: BlogPost) => ({
+    params: { id: post.id.toString() }
+  }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }: Iparam) {
+  return { props: { id: params.id } }
 }
 
 
